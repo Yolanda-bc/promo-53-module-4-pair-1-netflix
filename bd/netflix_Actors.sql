12534779c3fbefdbbf6939f7ff06cc826384a3ee
Yolanda-bc <jolandabc89@gmail.com>
2025-06-16 10:52:42 +0200

    add squema

--- bd/netflix_Actors.sql
+++ bd/netflix_Actors.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `netflix` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `netflix`;
 -- MySQL dump 10.13  Distrib 8.0.42, for macos15 (arm64)
 --
 -- Host: localhost    Database: netflix
@@ -50,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-15 21:47:06
+-- Dump completed on 2025-06-16 10:51:18
