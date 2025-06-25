ea49f70c9fbb2a4b0a770d5fd2715040ae081cdc
Yolanda-bc <jolandabc89@gmail.com>
2025-06-25 11:30:16 +0200

    add servidor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2/promise");
 
 // create and config server
 const server = express();
@@ -7,7 +8,24 @@ server.use(cors());
 server.use(express.json());
 
 // init express aplication
-const serverPort = 4000;
-server.listen(serverPort, () => {
-  console.log(`Server listening at http://localhost:${serverPort}`);
+const port = 4000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+async function getConnection() {
+  const datosConexion = {
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "password",
+    database: "netflix",
+  };
+  const conexion = await mysql.createConnection(datosConexion);
+  await conexion.connect();
+  return conexion;
+}
+server.get("/", (req, res) => {
+  console.log("Holis"); // -> Se ve en la terminal
+
+  res.send("Holis Adalabers!!!"); // -> Se ve en la página
 });
